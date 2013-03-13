1c509ae8d90e4c48545a4e6bb09a78595cac2d43
Thomas Kadauke <tkadauke@imedo.de>
2013-03-13 16:00:03 +0100

    Suggest translation for missing key if a translation with the same last part of the key name already exists

--- lib/i18n_tools/missing_scanner.rb
+++ lib/i18n_tools/missing_scanner.rb
@@ -21,27 +21,58 @@ module I18nTools
           current_hash = current_hash[part]
         end
         
-        if params.blank?
-          content = ""
-        else
-          keys = []
+        content = suggestion_for_key(parts.last) || ""
+        
+        if content.blank?
+          if params.blank?
+            content = ""
+          else
+            keys = []
           
-          parsed = params.split(",").map {|x|x.scan(/(:[a-zA-Z0-9_]+)\s*=>\s*(.*)/)}
-          parsed.each do |param|
-            next if param.blank?
+            parsed = params.split(",").map {|x|x.scan(/(:[a-zA-Z0-9_]+)\s*=>\s*(.*)/)}
+            parsed.each do |param|
+              next if param.blank?
             
-            key, value = *param.flatten
+              key, value = *param.flatten
             
-            keys << "%{#{key.sub(/^:/, '')}} (#{value})"
+              keys << "%{#{key.sub(/^:/, '')}} (#{value})"
+            end
+          
+            content = keys.join(", ")
           end
-          
-          content = keys.join(", ")
         end
         
         current_hash[parts.last] = content
       end
       
       result
+    end
+    
+    def suggestion_for_key(key)
+      root = I18n.backend.send(:lookup, @locale, "")
+      
+      candidates = find_key(key.to_sym, root)
+      
+      most_common_value(candidates) unless candidates.empty?
+    end
+  
+  private
+    def find_key(key, hash)
+      if hash[key] && hash[key].is_a?(String)
+        [hash[key]]
+      else
+        result = []
+        hash.each do |_, value|
+          result += find_key(key, value) if value.is_a?(Hash)
+        end
+        result
+      end
+    end
+    
+    def most_common_value(a)
+      a.group_by do |e|
+        e
+      end.values.max_by(&:size).first
     end
   end
 end
